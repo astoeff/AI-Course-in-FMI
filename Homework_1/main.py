88ffbce5d8f1321c2ba003c5b47ef85540008166
astoeff <antoni.1998@abv.bg>
2020-11-08 20:58:24 +0200

    Add final version for homework before submission

--- Homework_1/main.py
+++ Homework_1/main.py
@@ -21,7 +21,7 @@ def set_game_preconditions():
         row_as_list = [int(i) for i in r.split(',')]
         puzzle.append(row_as_list)
         if 0 in row_as_list:
-            zero_position_in_input_puzzle = (row, row_as_list.index(0))
+            zero_position_in_input_puzzle = (row_count, row_as_list.index(0))
         row_count += 1
 
     return (number_of_rows, index_of_zero_in_solved_puzzle, Puzzle_node(puzzle, zero_position_in_input_puzzle))
@@ -38,7 +38,6 @@ def find_puzzle_children(puzzle, final_state_dict_values):
         try:
             content[puzzle_zero_x][puzzle_zero_y], content[puzzle_zero_x - 1][puzzle_zero_y] = content[puzzle_zero_x - 1][puzzle_zero_y], content[puzzle_zero_x][puzzle_zero_y]
         except Exception as e:
-            # print('UP')
             pass
         else:
             puzzle_children.append(Puzzle_node(content, (puzzle_zero_x - 1, puzzle_zero_y), puzzle.distance_from_root + 1, puzzle, movement_direction = "down"))
@@ -51,7 +50,6 @@ def find_puzzle_children(puzzle, final_state_dict_values):
         try:
             content[puzzle_zero_x][puzzle_zero_y], content[puzzle_zero_x][puzzle_zero_y - 1] = content[puzzle_zero_x ][puzzle_zero_y - 1], content[puzzle_zero_x][puzzle_zero_y]
         except Exception as e:
-            # print('LEFT')
             pass
         else:
             puzzle_children.append(Puzzle_node(content, (puzzle_zero_x, puzzle_zero_y - 1), puzzle.distance_from_root + 1, puzzle, movement_direction = "right"))
@@ -62,7 +60,6 @@ def find_puzzle_children(puzzle, final_state_dict_values):
     try:
         content[puzzle_zero_x][puzzle_zero_y], content[puzzle_zero_x + 1][puzzle_zero_y] = content[puzzle_zero_x + 1][puzzle_zero_y], content[puzzle_zero_x][puzzle_zero_y]
     except Exception as e:
-        # print('DOWN')
         pass
     else:
         puzzle_children.append(Puzzle_node(content, (puzzle_zero_x + 1, puzzle_zero_y), puzzle.distance_from_root + 1, puzzle, movement_direction = "up"))
@@ -73,7 +70,6 @@ def find_puzzle_children(puzzle, final_state_dict_values):
     try:
         content[puzzle_zero_x][puzzle_zero_y], content[puzzle_zero_x][puzzle_zero_y + 1] = content[puzzle_zero_x][puzzle_zero_y + 1], content[puzzle_zero_x][puzzle_zero_y]
     except Exception as e:
-        # print('RIGHT')
         pass
     else:
         puzzle_children.append(Puzzle_node(content, (puzzle_zero_x, puzzle_zero_y + 1), puzzle.distance_from_root + 1, puzzle, movement_direction = "left"))
@@ -85,22 +81,12 @@ def find_puzzle_children(puzzle, final_state_dict_values):
 def iterative_deepening_a_star(root_puzzle, final_state, final_state_dict_values):
     is_solved = False
     limit = root_puzzle.calculate_heuristic(final_state_dict_values)
-    # count = 0
     while not is_solved:
-        # count+=1
-        # if count > 10:
-        #     break
         next_limit = limit + 2
-        potential_limit = limit + 1
-        #print("#################################### Limit: ", limit)
         stack = [root_puzzle]
         visited = [root_puzzle]
         while stack:
             current_puzzle = stack.pop()
-            # print('#############')
-            # current_puzzle.print_puzzle()
-            # print(current_puzzle.calculate_heuristic(final_state_dict_values))
-            # print('###########')
             if is_puzzle_solved(current_puzzle, final_state):
                 is_solved = True
                 final_state = current_puzzle
@@ -111,19 +97,7 @@ def iterative_deepening_a_star(root_puzzle, final_state, final_state_dict_values
                 if  child.calculate_heuristic(final_state_dict_values) < limit and child not in visited:
                     stack.append(child)
                     visited.append(child)
-                    # break
-                # if next_limit > child.heuristic and child.heuristic > limit:
-                # if limit < child.heuristic:
-                #     if next_limit == potential_limit:
-                #         next_limit = child.heuristic + 1
-                #     else:
-                #         next_limit = min(next_limit, child.heuristic + 1)
-        # if next_limit != potential_limit:
-        #     limit = next_limit
-        # else:
-        #     limit = potential_limit
         limit = next_limit
-    # print("-------------------------REVERSED------------------------------------------")
 
     father = final_state.father_node
     steps = 0
@@ -131,11 +105,8 @@ def iterative_deepening_a_star(root_puzzle, final_state, final_state_dict_values
     if final_state.movement_direction:
         directions.append(final_state.movement_direction)
     while father:
-        # print('-----------------------------')
-        # father.print_puzzle()
         if father.movement_direction:
             directions.append(father.movement_direction)
-        # print('-----------------------------')
         father = father.father_node
         steps += 1
 
@@ -149,15 +120,8 @@ def print_puzzle_solution(steps, directions):
         print(directions[i])
         i -= 1
 
-# def ida_star(root_puzzle, final_state, final_state_dict_values):
-#     is_solved = False
-#     while not is_solved:
-#         is_solved = alg(root_puzzle, final_state, final_state_dict_values, limit)
-
-# def alg():
-#     pass
-
 def is_puzzle_solvable(puzzle):
+    #TODO: check if the puzzle is solvable
     return True
 
 def create_final_state_of_puzzle_from_given_size_and_zero_position(size, zero_position):
@@ -177,34 +141,19 @@ def is_puzzle_solved(puzzle, final_state):
     return puzzle == final_state
 
 if __name__ == "__main__":
-    # game_attributes = set_game_preconditions()
+    game_attributes = set_game_preconditions()
 
     #3 for 3x3, 4 for 4x4 and 5 for 5x5
-    # size = game_attributes[0]
-    # index_of_zero_in_solved_puzzle = game_attributes[1]
-    # puzzle = game_attributes[2]
+    size = game_attributes[0]
+    index_of_zero_in_solved_puzzle = game_attributes[1]
+    puzzle = game_attributes[2]
    
-    # puzzle = Puzzle_node([[1, 2, 5], [7, 6, 4], [8, 0, 3]], (2,1))
-    # puzzle = Puzzle_node([[0, 2, 6], [1, 5, 3], [7, 4, 8]], (0,0))
-    start_time = time.time()
-    #final_state = create_final_state_of_puzzle_from_given_size_and_zero_position(4, -1)
-    final_state = create_final_state_of_puzzle_from_given_size_and_zero_position(3, -1)
+    final_state = create_final_state_of_puzzle_from_given_size_and_zero_position(size, index_of_zero_in_solved_puzzle)
 
-    # fn = Puzzle_node(final_state, (index_of_zero_in_solved_puzzle / size, index_of_zero_in_solved_puzzle % size))
-    #fn = Puzzle_node(final_state, (3, 3))
-    fn = Puzzle_node(final_state, (2, 2))
+    final_state_puzzle = Puzzle_node(final_state, (index_of_zero_in_solved_puzzle / size, index_of_zero_in_solved_puzzle % size))
 
-    final_state_dict_values = fn.tile_number_position_dict
-    puzzle = Puzzle_node([[1, 2, 6], [0, 5, 3], [7, 4, 8]], (1,0))
-    #puzzle = Puzzle_node([[5, 6, 3, 4], [8, 0, 1, 15], [10, 7, 2, 11], [12, 9, 14, 13]], (1,1))
+    final_state_dict_values = final_state_puzzle.tile_number_position_dict
 
-
-    # puzzle3 = Puzzle_node([[1,2,3], [0,5,6], [4,7,8]], (1,0))
-    # puzzle4 = Puzzle_node([[1,2,3,4], [5,6,7,8], [0,10,11,12], [9,13,14,15]], (2,0))
-    # puzzle5 = Puzzle_node([[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [0,17,18,19,20], [16, 21, 22, 23, 24]], (3,0))
-    # print(is_puzzle_solved(puzzle, fn))
-    #print(puzzle.calculate_manhattan_distance(fn))
-    steps, directions = iterative_deepening_a_star(puzzle, fn, final_state_dict_values)
+    steps, directions = iterative_deepening_a_star(puzzle, final_state_puzzle, final_state_dict_values)
     print_puzzle_solution(steps, directions)
-    # assert puzzle.is_solvable, "Puzzle is not solvable!"
-    print("--- %s seconds ---" % (time.time() - start_time))
+    assert puzzle.is_solvable, "Puzzle is not solvable!"
--- Homework_1/puzzle_node.py
+++ Homework_1/puzzle_node.py
@@ -1,7 +1,5 @@
 from math import sqrt
 
-# from constants import SOLVED_PUZZLES_TILES_POSITIONS, FINAL_PUZZLE_STATES
-
 class Puzzle_node():
     def __init__(self, content, zero_position, distance_from_root = 0, father_node = None, movement_direction = None):
         self.content = content
@@ -10,20 +8,6 @@ class Puzzle_node():
         self.father_node = father_node
         self.children = None
         self.movement_direction = movement_direction
-        # self.final_state = final_state
-
-    # @property
-    # def manhattan_distance(self):
-    #     manhattan_distance = 0
-    #     tile_x = 0
-    #     for row in self.content:
-    #         tile_y = 0
-    #         for tile in row:
-    #             if tile != 0:
-    #                 manhattan_distance += abs(tile_x - SOLVED_PUZZLES_TILES_POSITIONS[self.size][tile][0]) + abs(tile_y - SOLVED_PUZZLES_TILES_POSITIONS[self.size][tile][1])
-    #             tile_y += 1
-    #         tile_x += 1
-    #     return manhattan_distance
 
     def calculate_manhattan_distance(self, final_state_dict_values):
         lst_values_of_self = list(self.tile_number_position_dict.values())
@@ -31,16 +15,7 @@ class Puzzle_node():
         return  sum([abs(lst_values_of_self[i][0] - lst_values_of_final_state[i][0])\
                     + abs(lst_values_of_self[i][1] - lst_values_of_final_state[i][1]) for i in range(1, self.size * self.size)])
 
-        # m = 0
-        # vals1 = list(self.tile_number_position_dict.values())
-        # vals2 = list(final_state.tile_number_position_dict.values())
-        # print(vals1)
-        # print(vals2)
-        # for i in range(0, self.size*self.size):
-        #    m += self.
-
     def calculate_heuristic(self, final_state_dict_values):
-        # print(self.calculate_manhattan_distance(final_state))
         return self.distance_from_root + self.calculate_manhattan_distance(final_state_dict_values)
 
     @property
@@ -55,14 +30,9 @@ class Puzzle_node():
             tile_x += 1
         return dict(sorted(tile_number_position_dict.items()))
     
-
     @property
     def size(self):
         return len(self.content[0])
-
-    @property
-    def is_solved(self):
-        return self.content == FINAL_PUZZLE_STATES[self.size]
 
     @property
     def is_solvable(self):
