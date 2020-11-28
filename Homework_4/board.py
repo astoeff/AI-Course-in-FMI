93f096c65223f4bb8e52ec32b40beadcdce93cee
astoeff <antoni.1998@abv.bg>
2020-11-28 13:00:19 +0200

    Add player move

--- Homework_4/board.py
+++ Homework_4/board.py
@@ -5,6 +5,9 @@ class Board():
 	def __init__(self, board):
 		self.board = board
 
+	def update(self, position, sign):
+		self.board[position[0]][position[1]] = sign
+
 	def print(self):
 		print(UPPER_LABEL_OF_PRINTING_BOARD_STRING)
 		count = 0
--- Homework_4/main.py
+++ Homework_4/main.py
@@ -1,8 +1,8 @@
-from constants import PLAYER_IS_FIRST_VALUE, COMPUTER_IS_FIRST_VALUE, INITIAL_VERSION_OF_BOARD
+from constants import PLAYER_IS_FIRST_VALUE, COMPUTER_IS_FIRST_VALUE, INITIAL_VERSION_OF_BOARD, EMPTY_POSITION_ON_BOARD_CHAR, PLAYER_SIGN_ON_BOARD_CHAR
 from board import Board
 
 
-def read_input_from_console():
+def read_who_is_first_input_from_console():
     inputted_value = None
     is_inputted_value_valid = inputted_value == PLAYER_IS_FIRST_VALUE or inputted_value == COMPUTER_IS_FIRST_VALUE
     while not is_inputted_value_valid:
@@ -13,12 +13,30 @@ def read_input_from_console():
 def check_if_player_is_first(inputted_value):
     return inputted_value == PLAYER_IS_FIRST_VALUE
 
+def read_player_position_input_from_console():
+    inputted_value = str(input('Enter position in format x,y, example: 0, 2: ' ))
+    position = (int(inputted_value[0]), int(inputted_value[len(inputted_value) - 1]))
+    return position
+
+def check_if_position_is_available(board, position):
+    return board.board[position[0]][position[1]] == EMPTY_POSITION_ON_BOARD_CHAR
+
+def player_moves(board):
+    board.print()
+    position = None
+    is_position_available = False
+    while not is_position_available:
+        position = read_player_position_input_from_console()
+        is_position_available = check_if_position_is_available(board, position)
+    board.update(position, PLAYER_SIGN_ON_BOARD_CHAR)
+    # board.print()
+
 def main():
-    inputted_value = read_input_from_console()
+    inputted_value = read_who_is_first_input_from_console()
     is_player_first = check_if_player_is_first(inputted_value)
     initial_board_list_of_values = INITIAL_VERSION_OF_BOARD
     board = Board(initial_board_list_of_values)
-    board.print()
+    player_moves(board)
     
 if __name__ == '__main__':
     main()
